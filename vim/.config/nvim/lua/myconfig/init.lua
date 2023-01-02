ad1bebb2025f2f7bfac11f2979695fff2e021d02
Atthavit Wannasakwong <atthavit@gmail.com>
2023-01-02 18:00:46 +0700

    remove asdf

--- vim/.config/nvim/lua/myconfig/init.lua
+++ vim/.config/nvim/lua/myconfig/init.lua
@@ -70,7 +70,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require 'lspconfig'
 local servers = { 'pyright', 'yamlls', 'tsserver' }
 for _, lsp in ipairs(servers) do
